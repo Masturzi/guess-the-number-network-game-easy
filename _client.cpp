2fb37489e3a19742a161bbd6900391a5671bc44b
Masturzi <107308078+Masturzi@users.noreply.github.com>
2022-12-06 19:15:05 +0800

    Create _client.cpp

---
+++ _client.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    // Create a socket
+    int sockfd = std::socket(AF_INET, SOCK_STREAM, 0);
+
+    // Connect to the server
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(369);
+    addr.sin_addr.s_addr = std::inet_addr("127.0.0.1");
+    std::connect(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+
+    // Loop until the user guesses the number
+    while (true)
+    {
+        // Prompt the user to enter a guess
+        std::cout << "Enter a guess: ";
+        int guess;
+        std::cin >> guess;
+
+        // Send the guess to the server
+        char buffer[1024];
+        std::sprintf(buffer, "%d", guess);
+        std::write(sockfd, buffer, std::strlen(buffer));
+
+        // Read the response from the server
+        std::memset(buffer, 0, sizeof(buffer));
+        int n = std::read(sockfd, buffer, sizeof(buffer));
+
+        // Print the response from the server
+        std::cout << buffer;
+
+        // Check if the user won
+        if (std::strstr(buffer, "You won") != NULL)
+        {
+            // Close the socket
+            std::close(sockfd);
+
+            // Exit the loop
+            break;
+        }
+    }
+
+    return 0;
+}
